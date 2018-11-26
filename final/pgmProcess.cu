510577260e512006126229ea45f34da8d307f291
greg757 <41590361+glarrick757@users.noreply.github.com>
2018-11-26 11:49:48 -0800

    Added stack implementation

--- final/pgmProcess.cu
+++ final/pgmProcess.cu
@@ -10,6 +10,48 @@
  *  @return         return distance between p1 and p2
  */
  
+//A structure to represent a stack 
+struct Stack 
+{ 
+    int top; 
+    unsigned capacity; 
+    int* array; 
+}; 
+  
+//function to create a stack of given capacity. It initializes size of stack as 0 
+__device__ struct Stack* createStack(unsigned capacity) 
+{ 
+    struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack)); 
+    stack->capacity = capacity; 
+    stack->top = -1; 
+    stack->array = (int*) malloc(stack->capacity * sizeof(int)); 
+    return stack; 
+} 
+  
+// Stack is full when top is equal to the last index 
+__device__ int isFull(struct Stack* stack) 
+{   return stack->top == stack->capacity - 1; } 
+  
+// Stack is empty when top is equal to -1 
+__device__ int isEmpty(struct Stack* stack) 
+{   return stack->top == -1;  } 
+  
+// Function to add an item to stack.  It increases top by 1 
+__device__ void push(struct Stack* stack, int item) 
+{ 
+    if (isFull(stack)) 
+        return; 
+    stack->array[++stack->top] = item; 
+} 
+  
+// Function to remove an item from stack.  It decreases top by 1 
+__device__ int pop(struct Stack* stack) 
+{ 
+    if (isEmpty(stack)) 
+        return INT_MIN; 
+    return stack->array[stack->top--]; 
+}
+
 __global__ void gaussianBlur(int *d_pixelsIn, int *d_pixelsOut, double *guassian, int floatpitch, int width) {
 	extern __shared__ int s_data[];
   
